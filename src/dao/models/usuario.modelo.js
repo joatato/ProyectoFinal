52951699809468e230593c5ce8ac009439cbeeb9
Joaquín Agüero <joaquintatoaguero@gmail.com>
2023-05-16 17:15:44 -0300

    Detalle

--- src/dao/models/usuario.modelo.js
+++ src/dao/models/usuario.modelo.js
@@ -1,11 +1,11 @@
 import { Schema, model } from "mongoose";
 
-const usuarioColeccion='usuarios';
-const usuarioEsquema=new Schema({
-    nombre: String, apellido: String, 
-    email: {type: String, unique:true},
+const usuarioColeccion = 'usuarios';
+const usuarioEsquema = new Schema({
+    nombre: String, apellido: String,
+    email: { type: String, unique: true },
     password: String,
-    edad:Number
+    edad: Number
 });
 
-export const usuarioModelo=model(usuarioColeccion, usuarioEsquema);
+export const usuarioModelo = model(usuarioColeccion, usuarioEsquema);
